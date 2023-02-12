ae0ad9b95802fed1b0125909772a82e845eea986
Skkomolkina <skomolkina@gmail.com>
2023-02-12 19:36:23 +0300

    feat: add Async lazy load

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {Suspense} from 'react';
+import {Route, Routes} from 'react-router-dom';
 
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
 
 import './styles/index.scss';
+import {AboutPage} from "pages/AboutPage";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
@@ -11,6 +13,12 @@ const App = () => {
     return (
         <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>TOGGLE</button>
+
+            <Suspense fallback={<div>Загрузка</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
